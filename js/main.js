1a31efab9fbe3e876f7104072715bca29d2e6d56
Serge <waxqo69@gmail.com>
2024-01-18 16:46:54 +0100

    tt

--- js/main.js
+++ js/main.js
@@ -3,41 +3,27 @@ var imageLeft;
 var treeOfLifeImage;
 var eyeOfHorusImage;
 
-document.addEventListener("DOMContentLoaded", function(){
-    zombieImage = document.getElementById('zombie');
-    imageLeft = document.getElementById('image-left');
-    treeOfLifeImage = document.getElementById('tree-of-life');
-    eyeOfHorusImage = document.getElementById('eye-of-horus');
+document.addEventListener('DOMContentLoaded', function () {
+    var menuToggle = document.getElementById('menuToggle');
+    var menuList = document.getElementById('menuList');
 
-    zombieImage.addEventListener('mouseover', function() {
-        zombieImage.style.filter = 'grayscale(100%)';
+    menuToggle.addEventListener('click', function () {
+        event.preventDefault();
+        menuList.classList.toggle('show');
     });
+});
 
-    zombieImage.addEventListener('mouseout', function() {
-        zombieImage.style.filter = 'none';
-    });
+document.addEventListener("DOMContentLoaded", function () {
 
-    imageLeft.addEventListener('mouseover', function() {
-        imageLeft.style.filter = 'grayscale(100%)';
-    });
+    zombieImage = document.getElementsByClassName('blackwhite');
+    
+    for(const img of zombieImage){
+        img.addEventListener('mouseover', function () {
+            this.style.filter = 'grayscale(100%)';
+        });
 
-    imageLeft.addEventListener('mouseout', function() {
-        imageLeft.style.filter = 'none'; 
-    });
-
-    treeOfLifeImage.addEventListener('mouseover', function() {
-        treeOfLifeImage.style.filter = 'grayscale(100%)';
-    });
-
-    treeOfLifeImage.addEventListener('mouseout', function() {
-        treeOfLifeImage.style.filter = 'none';
-    });
-
-    eyeOfHorusImage.addEventListener('mouseover', function() {
-        eyeOfHorusImage.style.filter = 'grayscale(100%)';
-    });
-
-    eyeOfHorusImage.addEventListener('mouseout', function() {
-        eyeOfHorusImage.style.filter = 'none';
-    });
+        img.addEventListener('mouseout', function () {
+            this.style.filter = 'none';
+        });
+    }
 });
